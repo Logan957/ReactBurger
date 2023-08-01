0bfe89d9c685c918cd45c0b00ddea8fa76dae509
Роман Зайдулин <roman.zaidulin2015@gmail.com>
2023-08-01 10:12:42 +0300

    fix

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,6 +1,5 @@
 import {
   BurgerIcon,
-  Button,
   ListIcon,
   Logo,
   ProfileIcon,
@@ -10,35 +9,37 @@ import styles from "./app-header.module.css";
 
 const AppHeader = () => {
   return (
-    <header className={`${styles.header}`}>
-      <div className={`${styles.constructor_button}`}>
-        <a className="text-decoration-none" href="">
-          <BurgerIcon type="primary" />
-          <span
-            className={`${styles.text_white} ms-2 text text_type_main-default`}
-          >
-            Конструктор
-          </span>
-        </a>
-        <a href="" className="ml-8 text-decoration-none">
-          <ListIcon type="secondary" />
+    <header className={`${styles.header} d-flex justify-content-center`}>
+      <div className={`${styles.content} d-flex justify-content-between`}>
+        <div className={`${styles.constructor_button}`}>
+          <a className="text-decoration-none" href="">
+            <BurgerIcon type="primary" />
+            <span
+              className={`${styles.text_white} ms-2 text text_type_main-default`}
+            >
+              Конструктор
+            </span>
+          </a>
+          <a href="" className="ml-8 text-decoration-none">
+            <ListIcon type="secondary" />
+            <span className="ms-2 text text_type_main-default text_color_inactive">
+              Лента заказа
+            </span>
+          </a>
+        </div>
+        <div className={`${styles.logo}`}>
+          <Logo />
+        </div>
+        <a
+          href=""
+          className={`${styles.personal_area_button} text-decoration-none`}
+        >
+          <ProfileIcon type="secondary" />
           <span className="ms-2 text text_type_main-default text_color_inactive">
-            Лента заказа
+            Личный кабинет
           </span>
         </a>
       </div>
-      <div className={`${styles.logo}`}>
-        <Logo />
-      </div>
-      <a
-        href=""
-        className={`${styles.personal_area_button} text-decoration-none`}
-      >
-        <ProfileIcon type="secondary" />
-        <span className="ms-2 text text_type_main-default text_color_inactive">
-          Личный кабинет
-        </span>
-      </a>
     </header>
   );
 };
